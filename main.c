7f6d5a540200fe8e38f6d2a5a6b8db90f60c2bba
juliana008 <julibarrettovb@gmail.com>
2024-09-12 20:16:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -25,13 +25,105 @@ int CriarFroakie(pokemon *p){
     
 }
 
-/*int CriarPikachu(pokemon *p){
-    p->nome='Pikachu';
+int CriarMudkip(pokemon *p){
+    p->nome='Mudkip';
     p->vida = 
     p->h = 
     p-> catchrate = 
     
-}*/
+}
+
+int CriarSquirtle(pokemon *p){
+    p->nome='Squirtle';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarTorchic(pokemon *p){
+    p->nome='Torchic';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarCyndaquil(pokemon *p){
+    p->nome='Cindaquil';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarFuecoco(pokemon *p){
+    p->nome='Fuecoco';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarChikorita(pokemon *p){
+    p->nome='Chikorita';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarTurtwig(pokemon *p){
+    p->nome='Turtwig';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarTreecko(pokemon *p){
+    p->nome='Treecko';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarGrimer(pokemon *p){
+    p->nome='Grimer';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarEvans(pokemon *p){
+    p->nome='Evans';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarKoffin(pokemon *p){
+    p->nome='Koffing';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarSandshrew(pokemon *p){
+    p->nome='Sandshrew';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarDiglett(pokemon *p){
+    p->nome='Diglett';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
+int CriarCubone(pokemon *p){
+    p->nome='Cubone';
+    p->vida = 
+    p->h = 
+    p-> catchrate = 
+    
+}
 
 //cria todos os pokemons aqui
 
